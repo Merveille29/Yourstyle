35e604e41c3435fe8bef4c0954153fd4651187e0
Merveille29 <merveille.gangni@epitech.eu>
2023-08-09 12:32:05 +0100

    yourstyle website session1

---
+++ yourstyle/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container-fluid bg1">
+      <div class="">
+        <div>
+          <button class="btn font-h mt-10" type="submit">Home</button>
+          <button class="btn font-2" type="submit">Shop</button>
+          <button class="log">YOUR</button>
+          <button class="log1">STYLE</button>
+          <span class="inline-flex items-baseline">
+            <button class="btn font-3" type="submit">Categories</button>
+            <button class="btn font-4" type="submit">About</button>
+            <img class="img14" src="./assets/imag/img14.png" width="18" alt="" />
+            <img class="img15" src="./assets/imag/img15.png" width="18" alt="" />
+          </span></div>
+      <div class="rec1"></div>
+      </div>
+      <div class="relative flex flex-col">
+        <div class="z-50 img1">
+          <img class="-my-[17%]" src="./assets/imag/img1.png" width="380" alt="" />
+        </div>
+        <div class="z-0 flex items-start -my-[25%] pl-21">
+          <h1 class="log2">YOUR</h1>
+          <h1 class="log3">STYLE</h1>
+        </div>
+        <h1 class="text-gray-600 my-[23%] tex">Create your own visual style. Let it be unique for <br> yourself and yet identifiable for others. Check <br> out our collection to identify who you are.</h1>
+      </div>
+      <div class="flex card -my-[24%]">
+          <div class="card1">
+            <img class="img5" src="./assets/imag/img5.png" width="122" alt="" />
+          </div>
+          <div class="-my-[6%]">
+            <div class="flex items-start space-x-36">
+              <div class="slide2"><p class="my-[15%]">&#60</p></div>
+              <div class="slide1"><p class="my-[15%]">></p></div>
+            </div>
+            <div class="card2">
+              <img class="img6" src="./assets/imag/img6.png" width="130" alt="" />
+            </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+@import url("./assets/css/style.css");
+</style>
